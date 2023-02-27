0b8772b567ab16d8e6d5a218c5ea176305867916
nathanpuckett <nathanfpuckett@gmail.com>
2023-02-27 16:53:08 -0600

    Create get_move method

--- main.rb
+++ main.rb
@@ -9,31 +9,42 @@ class Board
 
   def initialize
     @board = [
-      ['   a     b     c  '], ['      |     |     '],
-      ['1  -  |  -  |  -  '], [' _____|_____|_____'],
-      ['      |     |     '], ['2  -  |  -  |  -  '],
+      ['   0     1     2  '], ['      |     |     '],
+      ['0  -  |  -  |  -  '], [' _____|_____|_____'],
+      ['      |     |     '], ['1  -  |  -  |  -  '],
       [' _____|_____|_____'], ['      |     |     '],
-      ['3  -  |  -  |  -  '], ['      |     |     ']
+      ['2  -  |  -  |  -  '], ['      |     |     ']
     ]
-    @moves = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+    @moves = [
+      ['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']
+    ]
+  end
+
+  def get_move
+    print 'Please enter your move: '
+    @move = gets.chomp.split('')
+    @moves[@move[1].to_i][@move[2].to_i] = @move[0]
+    p @moves
   end
 
   def refresh_board
-    @board[2][0][3] = self.moves[0]
-    @board[2][0][9] = self.moves[1]
-    @board[2][0][15] = self.moves[2]
-    @board[5][0][3] = self.moves[3]
-    @board[5][0][9] = self.moves[4]
-    @board[5][0][15] = self.moves[5]
-    @board[8][0][3] = self.moves[6]
-    @board[8][0][9] = self.moves[7]
-    @board[8][0][15] = self.moves[8]
+    @board[2][0][3] = self.moves[0][0]
+    @board[2][0][9] = self.moves[0][1]
+    @board[2][0][15] = self.moves[0][2]
+    @board[5][0][3] = self.moves[1][0]
+    @board[5][0][9] = self.moves[1][1]
+    @board[5][0][15] = self.moves[1][2]
+    @board[8][0][3] = self.moves[2][0]
+    @board[8][0][9] = self.moves[2][1]
+    @board[8][0][15] = self.moves[2][2]
   end
 end
 
 game = Board.new
 
-game.moves[0] = 'X'
+game.get_move
+
+# game.moves[0][0] = 'X'
 
 game.refresh_board
 
